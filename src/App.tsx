afb1db320a3c4c8bd1404fea41211316482bd70f
Soma Sekine <a8121151@aoyama.jp>
2025-08-12 15:42:01 +0900

    cube表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
-import "./App.css";
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const setup = () => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+      const renderer = new THREE.WebGLRenderer({ canvas });
+      renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      const frame = () => {
+        renderer.render(scene, camera);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        requestAnimationFrame(frame);
+      };
+      requestAnimationFrame(frame);
+    };
+
+    setup();
+  }, []);
+
   return (
     <>
-      <p>Hello</p>
+      <p>Hello mate</p>
+      <canvas ref={canvasRef} width={500} height={500}></canvas>
     </>
   );
 }
