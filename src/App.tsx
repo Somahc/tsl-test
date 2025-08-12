a49fcb2d3cac565926be5ef2195d0c3490542901
Soma Sekine <a8121151@aoyama.jp>
2025-08-12 17:30:30 +0900

    NodeMaterialでCubeをレンダリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { MeshStandardNodeMaterial, WebGPURenderer } from "three/webgpu";
+import { uniform, float, color } from "three/tsl";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
@@ -17,11 +19,22 @@ function App() {
         1000
       );
 
-      const renderer = new THREE.WebGLRenderer({ canvas });
+      const renderer = new WebGPURenderer({ canvas });
       renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      // ライト
+      const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+      scene.add(ambient);
+
+      const dir = new THREE.DirectionalLight(0xffffff, 1.2);
+      dir.position.set(3, 3, 3);
+      scene.add(dir);
+
+      const geometry = new THREE.BoxGeometry(2, 2, 2);
+      // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const material = new MeshStandardNodeMaterial();
+      material.colorNode = uniform(material.color).mul(color(1.0, 0, 0.0));
+      material.roughnessNode = float(0.2);
       const cube = new THREE.Mesh(geometry, material);
       scene.add(cube);
 
@@ -41,7 +54,7 @@ function App() {
 
   return (
     <>
-      <p>Hello mate</p>
+      <p>NodeMaterialテスト</p>
       <canvas ref={canvasRef} width={500} height={500}></canvas>
     </>
   );
