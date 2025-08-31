3546311f6ff04622adee85d4e7d080e463d07a83
Soma Sekine <a8121151@aoyama.jp>
2025-08-31 11:16:44 +0900

    tutorial step2の途中まで
    ColorPreviewノードのHandleがRGB別でそれぞれ定義してしまっているので、次のImproving the codeの章で一つにまとめる必要がある

--- src/routes/flow-tutorial/ColorPreview/ColorPreview.tsx
+++ src/routes/flow-tutorial/ColorPreview/ColorPreview.tsx
@@ -1,56 +1,97 @@
-import { Handle, Position, useNodeConnections, useNodesData, useReactFlow, type Node, type NodeProps } from "@xyflow/react"
-import { useCallback, useEffect } from "react";
+import {
+  Handle,
+  Position,
+  useNodeConnections,
+  useNodesData,
+  useReactFlow,
+  type Node,
+  type NodeProps,
+} from "@xyflow/react";
+import { useCallback, useEffect, useState } from "react";
 
 type Props = {
-    handleId: string;
-    nodeId: string;
-    label: string;
+  id: string;
+  label: string;
+};
+
+function CustomHandle({ id, label, onChange }: Props) {
+  const connections = useNodeConnections({
+    handleType: "target",
+    handleId: id,
+  });
+
+  const nodeData = useNodesData<Node<{ value: number }>>(
+    connections?.[0]?.source
+  );
+
+  useEffect(() => {
+    onChange(nodeData?.data ? nodeData.data.value : 0);
+  }, [nodeData]);
+
+  // useEffect(() => {
+  //   const incomingValue = nodeData?.data?.value;
+  //   onChange(typeof incomingValue === "number" ? incomingValue : 0);
+  // }, [nodeData, onChange]);
+
+  return (
+    <div>
+      <Handle type="target" position={Position.Left} id={id} />
+      <label>{label}</label>
+    </div>
+  );
 }
 
-function CustomHandle({ handleId, nodeId, label, onChange }: Props) {
-    const connections = useNodeConnections({
-        handleType: 'target',
-        handleId,
-        id: nodeId,
-    })
+type RGB = { r: number; g: number; b: number };
+export type ColorPreviewNode = Node<{ value?: RGB }, "ColorPreview">;
 
-    const nodeData = useNodesData<Node<{ value: number }>>(connections?.[0]?.source)
+export default function ColorPreview({
+  id,
+  data,
+}: NodeProps<ColorPreviewNode>) {
+  const redConnections = useNodeConnections({
+    handleType: "target",
+    handleId: "red",
+  });
+  const redNodeData = useNodesData(redConnections?.[0].source);
 
+  const greenConnections = useNodeConnections({
+    handleType: "target",
+    handleId: "green",
+  });
+  const greenNodeData = useNodesData(greenConnections?.[0].source);
 
+  const blueConnections = useNodeConnections({
+    handleType: "target",
+    handleId: "blue",
+  });
+  const blueNodeData = useNodesData(blueConnections?.[0].source);
 
-    useEffect(() => {
-        const incomingValue = nodeData?.data?.value
-        onChange(typeof incomingValue === 'number' ? incomingValue : 0)
-    }, [nodeData, onChange])
+  const color = {
+    r: redNodeData?.data ? redNodeData.data.value : 0,
+    g: greenNodeData?.data ? greenNodeData.data.value : 0,
+    b: blueNodeData?.data ? blueNodeData.data.value : 0,
+  };
 
-    return (
+  return (
+    <>
+      <div
+        style={{
+          background: `rgb(${color.r}, ${color.g}, ${color.b})`,
+        }}
+      >
         <div>
-            <Handle type="target" position={Position.Left} id={handleId} />
-            <label htmlFor={handleId}>{label}</label>
+          <Handle type="target" position={Position.Left} id="red" />
+          <label>R</label>
         </div>
-    )
-
-}
-
-type RGB = { r: number; g: number; b: number }
-export type ColorPreviewNode = Node<{ value?: RGB }, 'ColorPreview'>
-
-export default function ColorPreview({ id, data }: NodeProps<ColorPreviewNode>) {
-    const { updateNodeData } = useReactFlow();
-
-    const onChange = useCallback((handleId: string, value: number) => {
-        console.log(handleId, value)
-        updateNodeData(id, (node) => {
-            return { value: { ...(node.data?.value ?? { r: 0, g: 0, b: 0 }), [handleId.charAt(0)]: value } }
-        })
-    }, [id, updateNodeData])
-    return(
-        <>
-            <div style={{ background: data.value ? `rgb(${data.value.r}, ${data.value.g}, ${data.value.b})` : 'rgb(0, 0, 0)',}}>
-                <CustomHandle handleId="red" nodeId={id} label="R" onChange={onChange} />
-                <CustomHandle handleId="green" nodeId={id} label="G" onChange={onChange} />
-                <CustomHandle handleId="blue" nodeId={id} label="B" onChange={onChange} />
-            </div>
-        </>
-    )
+        <div>
+          <Handle type="target" position={Position.Left} id="green" />
+          <label>G</label>
+        </div>
+        <div>
+          <Handle type="target" position={Position.Left} id="blue" />
+          <label>B</label>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/routes/flow-tutorial/CustomNodes/NumberInput.tsx
+++ src/routes/flow-tutorial/CustomNodes/NumberInput.tsx
@@ -1,26 +1,47 @@
-import { Handle, Position, type Node, type NodeProps } from "@xyflow/react";
+import {
+  Handle,
+  Position,
+  useReactFlow,
+  type Node,
+  type NodeProps,
+} from "@xyflow/react";
 import { useCallback, useState } from "react";
 
-export type NumberInputNode = Node<{
-    label: string
-    value: number
-}, 'NumberInput'>
+export type NumberInputNode = Node<
+  {
+    label: string;
+    value: number;
+  },
+  "NumberInput"
+>;
 
 export default function NumberInput({ id, data }: NodeProps<NumberInputNode>) {
-    const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(data.value);
+  const { updateNodeData } = useReactFlow();
 
-    const onChange = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
-        const cappedNumber = Math.round(Math.min(255, Math.max(0, Number(evt.target.value))))
-        setNumber(cappedNumber)
-    },[])
+  const onChange = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
+    const cappedNumber = Math.round(
+      Math.min(255, Math.max(0, Number(evt.target.value)))
+    );
+    setNumber(cappedNumber);
+    updateNodeData(id, { value: cappedNumber });
+  }, []);
 
-    return (
-        <>
-            <div>
-                <div>{data.label}</div>
-                <input type="text" id={`number-${id}`} name="number" min={0} max={255} value={number} onChange={onChange} />
-                <Handle type="source" position={Position.Right} />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <div>{data.label}</div>
+        <input
+          type="text"
+          id={`number-${id}`}
+          name="number"
+          min={0}
+          max={255}
+          value={number}
+          onChange={onChange}
+        />
+        <Handle type="source" position={Position.Right} />
+      </div>
+    </>
+  );
 }
